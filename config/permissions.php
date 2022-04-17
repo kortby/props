69839955f26bcc06ad9525cd46920635fee79c94
faycal <hasbellaoui.faycal@gmail.com>
2022-04-17 20:36:35 +0100

    ...

---
+++ config/permissions.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    'view-any-category',
+    'view-category',
+    'create-category',
+    'update-category',
+    'delete-category',
+    'restore-category',
+    'force-delete-category',
+
+    'view-any-maintenance',
+    'view-maintenance',
+    'create-maintenance',
+    'update-maintenance',
+    'delete-maintenance',
+    'restore-maintenance',
+    'force-delete-maintenance',
+
+    'view-any-property',
+    'view-property',
+    'create-property',
+    'update-property',
+    'delete-property',
+    'restore-property',
+    'force-delete-property',
+
+    'view-any-type',
+    'view-type',
+    'create-type',
+    'update-type',
+    'delete-type',
+    'restore-type',
+    'force-delete-type',
+
+    'view-any-unit',
+    'view-unit',
+    'create-unit',
+    'update-unit',
+    'delete-unit',
+    'restore-unit',
+    'force-delete-unit',
+
+
+];
--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -32,98 +32,24 @@ class PermissionsSeeder extends Seeder
 
         // create permissions
         // Category
-        Permission::create(['name' => 'view-any-category']);
-        Permission::create(['name' => 'view-category']);
-        Permission::create(['name' => 'create-category']);
-        Permission::create(['name' => 'update-category']);
-        Permission::create(['name' => 'delete-category']);
-        Permission::create(['name' => 'restore-category']);
-        Permission::create(['name' => 'force-delete-category']);
+        foreach (config('permissions') as $permission) {
 
-        // Maintenance
-        Permission::create(['name' => 'view-any-maintenance']);
-        Permission::create(['name' => 'view-maintenance']);
-        Permission::create(['name' => 'create-maintenance']);
-        Permission::create(['name' => 'update-maintenance']);
-        Permission::create(['name' => 'delete-maintenance']);
-        Permission::create(['name' => 'restore-maintenance']);
-        Permission::create(['name' => 'force-delete-maintenance']);
+            Permission::create(['name'=>$permission]);
 
-        // Property
-        Permission::create(['name' => 'view-any-property']);
-        Permission::create(['name' => 'view-property']);
-        Permission::create(['name' => 'create-property']);
-        Permission::create(['name' => 'update-property']);
-        Permission::create(['name' => 'delete-property']);
-        Permission::create(['name' => 'restore-property']);
-        Permission::create(['name' => 'force-delete-property']);
-
-        // Type
-        Permission::create(['name' => 'view-any-type']);
-        Permission::create(['name' => 'view-type']);
-        Permission::create(['name' => 'create-type']);
-        Permission::create(['name' => 'update-type']);
-        Permission::create(['name' => 'delete-type']);
-        Permission::create(['name' => 'restore-type']);
-        Permission::create(['name' => 'force-delete-type']);
-
-        // Unit
-        Permission::create(['name' => 'view-any-unit']);
-        Permission::create(['name' => 'view-unit']);
-        Permission::create(['name' => 'create-unit']);
-        Permission::create(['name' => 'update-unit']);
-        Permission::create(['name' => 'delete-unit']);
-        Permission::create(['name' => 'restore-unit']);
-        Permission::create(['name' => 'force-delete-unit']);
+        }
 
 
 
         // create roles and assign existing permissions
         $role1 = Role::create(['name' => 'app-manager']);
-        // role1 category
-        $role1->givePermissionTo('view-any-category');
-        $role1->givePermissionTo('view-category');
-        $role1->givePermissionTo('create-category');
-        $role1->givePermissionTo('update-category');
-        $role1->givePermissionTo('delete-category');
-        $role1->givePermissionTo('restore-category');
-        $role1->givePermissionTo('force-delete-category');
 
-        // role1 maintenance
-        $role1->givePermissionTo('view-any-maintenance');
-        $role1->givePermissionTo('view-maintenance');
-        $role1->givePermissionTo('create-maintenance');
-        $role1->givePermissionTo('update-maintenance');
-        $role1->givePermissionTo('delete-maintenance');
-        $role1->givePermissionTo('restore-maintenance');
-        $role1->givePermissionTo('force-delete-maintenance');
+        foreach (config('permissions') as $permission) {
 
-        // role1 property
-        $role1->givePermissionTo('view-any-property');
-        $role1->givePermissionTo('view-property');
-        $role1->givePermissionTo('create-property');
-        $role1->givePermissionTo('update-property');
-        $role1->givePermissionTo('delete-property');
-        $role1->givePermissionTo('restore-property');
-        $role1->givePermissionTo('force-delete-property');
+            $role1->givePermissionTo($permission);
 
-        // role1 type
-        $role1->givePermissionTo('view-any-type');
-        $role1->givePermissionTo('view-type');
-        $role1->givePermissionTo('create-type');
-        $role1->givePermissionTo('update-type');
-        $role1->givePermissionTo('delete-type');
-        $role1->givePermissionTo('restore-type');
-        $role1->givePermissionTo('force-delete-type');
+        }
 
-        // role1 unit
-        $role1->givePermissionTo('view-any-unit');
-        $role1->givePermissionTo('view-unit');
-        $role1->givePermissionTo('create-unit');
-        $role1->givePermissionTo('update-unit');
-        $role1->givePermissionTo('delete-unit');
-        $role1->givePermissionTo('restore-unit');
-        $role1->givePermissionTo('force-delete-unit');
+
 
 
         // gets all permissions via Gate::before rule; see AuthServiceProvider
