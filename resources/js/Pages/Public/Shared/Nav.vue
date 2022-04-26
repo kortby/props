fa42690a20206b715f4e7cdf4a41a8592c2601bb
kortby <kortby@gmail.com>
2022-04-26 00:26:17 -0500

    Welcome page

--- resources/js/Pages/Public/Shared/Nav.vue
+++ resources/js/Pages/Public/Shared/Nav.vue
@@ -1,116 +1,101 @@
 <template>
-    <div>
-        <Popover>
-            <div class="max-w-7xl mx-auto px-4 sm:px-6">
-                <nav
-                    class="relative flex items-center justify-between sm:h-10 md:justify-center"
-                    aria-label="Global"
+    <Popover>
+        <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+            <nav
+                class="relative flex items-center justify-between sm:h-10 lg:justify-start"
+                aria-label="Global"
+            >
+                <div
+                    class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0"
                 >
                     <div
-                        class="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0"
+                        class="flex items-center justify-between w-full md:w-auto"
                     >
-                        <div
-                            class="flex items-center justify-between w-full md:w-auto"
-                        >
-                            <a href="#">
-                                <span class="sr-only">Workflow</span>
-                                <img
-                                    class="h-8 w-auto sm:h-10"
-                                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                                    alt=""
-                                />
-                            </a>
-                            <div class="-mr-2 flex items-center md:hidden">
-                                <PopoverButton
-                                    class="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                                >
-                                    <span class="sr-only">Open main menu</span>
-                                    <MenuIcon
-                                        class="h-6 w-6"
-                                        aria-hidden="true"
-                                    />
-                                </PopoverButton>
-                            </div>
+                        <a href="#">
+                            <span class="sr-only">Workflow</span>
+                            <img
+                                class="h-8 w-auto sm:h-10"
+                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                            />
+                        </a>
+                        <div class="-mr-2 flex items-center md:hidden">
+                            <PopoverButton
+                                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                            >
+                                <span class="sr-only">Open main menu</span>
+                                <MenuIcon class="h-6 w-6" aria-hidden="true" />
+                            </PopoverButton>
                         </div>
                     </div>
-                    <div class="hidden md:flex md:space-x-10">
+                </div>
+                <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                    <a
+                        v-for="item in navigation"
+                        :key="item.name"
+                        :href="item.href"
+                        class="font-medium text-gray-500 hover:text-gray-900"
+                        >{{ item.name }}</a
+                    >
+                    <a
+                        href="/login"
+                        class="font-medium text-indigo-600 hover:text-indigo-500"
+                        >Log in</a
+                    >
+                </div>
+            </nav>
+        </div>
+
+        <transition
+            enter-active-class="duration-150 ease-out"
+            enter-from-class="opacity-0 scale-95"
+            enter-to-class="opacity-100 scale-100"
+            leave-active-class="duration-100 ease-in"
+            leave-from-class="opacity-100 scale-100"
+            leave-to-class="opacity-0 scale-95"
+        >
+            <PopoverPanel
+                focus
+                class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+            >
+                <div
+                    class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden"
+                >
+                    <div class="px-5 pt-4 flex items-center justify-between">
+                        <div>
+                            <img
+                                class="h-8 w-auto"
+                                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                alt=""
+                            />
+                        </div>
+                        <div class="-mr-2">
+                            <PopoverButton
+                                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                            >
+                                <span class="sr-only">Close main menu</span>
+                                <XIcon class="h-6 w-6" aria-hidden="true" />
+                            </PopoverButton>
+                        </div>
+                    </div>
+                    <div class="px-2 pt-2 pb-3 space-y-1">
                         <a
                             v-for="item in navigation"
                             :key="item.name"
                             :href="item.href"
-                            class="font-medium text-gray-500 hover:text-gray-900"
+                            class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                             >{{ item.name }}</a
                         >
                     </div>
-                    <div
-                        class="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0"
+                    <a
+                        href="#"
+                        class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
                     >
-                        <span class="inline-flex rounded-md shadow">
-                            <a
-                                href="#"
-                                class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50"
-                            >
-                                Log in
-                            </a>
-                        </span>
-                    </div>
-                </nav>
-            </div>
-
-            <transition
-                enter-active-class="duration-150 ease-out"
-                enter-from-class="opacity-0 scale-95"
-                enter-to-class="opacity-100 scale-100"
-                leave-active-class="duration-100 ease-in"
-                leave-from-class="opacity-100 scale-100"
-                leave-to-class="opacity-0 scale-95"
-            >
-                <PopoverPanel
-                    focus
-                    class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-                >
-                    <div
-                        class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden"
-                    >
-                        <div
-                            class="px-5 pt-4 flex items-center justify-between"
-                        >
-                            <div>
-                                <img
-                                    class="h-8 w-auto"
-                                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                                    alt=""
-                                />
-                            </div>
-                            <div class="-mr-2">
-                                <PopoverButton
-                                    class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                                >
-                                    <span class="sr-only">Close menu</span>
-                                    <XIcon class="h-6 w-6" aria-hidden="true" />
-                                </PopoverButton>
-                            </div>
-                        </div>
-                        <div class="px-2 pt-2 pb-3">
-                            <a
-                                v-for="item in navigation"
-                                :key="item.name"
-                                :href="item.href"
-                                class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                                >{{ item.name }}</a
-                            >
-                        </div>
-                        <a
-                            href="/login"
-                            class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
-                        >
-                            Log in here
-                        </a>
-                    </div>
-                </PopoverPanel>
-            </transition>
-        </Popover>
-    </div>
+                        Log in
+                    </a>
+                </div>
+            </PopoverPanel>
+        </transition>
+    </Popover>
 </template>
 <script>
 import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
@@ -138,4 +123,3 @@ export default {
     },
 };
 </script>
-<style></style>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,133 +1,121 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-    <div class="relative bg-gray-50 overflow-hidden">
-        <div
-            class="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full"
-            aria-hidden="true"
-        >
-            <div class="relative h-full max-w-7xl mx-auto">
-                <svg
-                    class="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
-                    width="404"
-                    height="784"
-                    fill="none"
-                    viewBox="0 0 404 784"
+    <div>
+        <div class="relative bg-white overflow-hidden">
+            <div class="max-w-7xl mx-auto">
+                <div
+                    class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
                 >
-                    <defs>
-                        <pattern
-                            id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-                            x="0"
-                            y="0"
-                            width="20"
-                            height="20"
-                            patternUnits="userSpaceOnUse"
-                        >
-                            <rect
-                                x="0"
-                                y="0"
-                                width="4"
-                                height="4"
-                                class="text-gray-200"
-                                fill="currentColor"
-                            />
-                        </pattern>
-                    </defs>
-                    <rect
-                        width="404"
-                        height="784"
-                        fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
-                    />
-                </svg>
-                <svg
-                    class="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
-                    width="404"
-                    height="784"
-                    fill="none"
-                    viewBox="0 0 404 784"
-                >
-                    <defs>
-                        <pattern
-                            id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
-                            x="0"
-                            y="0"
-                            width="20"
-                            height="20"
-                            patternUnits="userSpaceOnUse"
-                        >
-                            <rect
-                                x="0"
-                                y="0"
-                                width="4"
-                                height="4"
-                                class="text-gray-200"
-                                fill="currentColor"
-                            />
-                        </pattern>
-                    </defs>
-                    <rect
-                        width="404"
-                        height="784"
-                        fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"
-                    />
-                </svg>
+                    <svg
+                        class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+                        fill="currentColor"
+                        viewBox="0 0 100 100"
+                        preserveAspectRatio="none"
+                        aria-hidden="true"
+                    >
+                        <polygon points="50,0 100,0 50,100 0,100" />
+                    </svg>
+
+                    <Nav />
+
+                    <main
+                        class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
+                    >
+                        <div class="sm:text-center lg:text-left">
+                            <h1
+                                class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+                            >
+                                <span class="block xl:inline"
+                                    >Data to enrich your</span
+                                >
+                                {{ " " }}
+                                <span class="block text-indigo-600 xl:inline"
+                                    >online business</span
+                                >
+                            </h1>
+                            <p
+                                class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+                            >
+                                Anim aute id magna aliqua ad ad non deserunt
+                                sunt. Qui irure qui lorem cupidatat commodo.
+                                Elit sunt amet fugiat veniam occaecat fugiat
+                                aliqua.
+                            </p>
+                            <div
+                                class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start"
+                            >
+                                <div class="rounded-md shadow">
+                                    <a
+                                        href="#"
+                                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                                    >
+                                        Get started
+                                    </a>
+                                </div>
+                                <div class="mt-3 sm:mt-0 sm:ml-3">
+                                    <a
+                                        href="#"
+                                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+                                    >
+                                        Live demo
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </main>
+                </div>
+            </div>
+            <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+                <img
+                    class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+                    src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+                    alt=""
+                />
             </div>
         </div>
-
-        <div class="relative pt-6 pb-16 sm:pb-24">
-            <Nav />
-
-            <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
-                <div class="text-center">
-                    <h1
-                        class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
-                    >
-                        <span class="block xl:inline"
-                            >Software to manage your</span
-                        >
-                        {{ " " }}
-                        <span class="block text-indigo-600 xl:inline"
-                            >Properties</span
-                        >
-                    </h1>
-                    <p
-                        class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
-                    >
-                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui
-                        irure qui lorem cupidatat commodo. Elit sunt amet fugiat
-                        veniam occaecat fugiat aliqua.
-                    </p>
-                    <div
-                        class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8"
-                    >
-                        <div class="rounded-md shadow">
-                            <a
-                                href="#"
-                                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-                            >
-                                Get started
-                            </a>
-                        </div>
-                        <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                            <a
-                                href="#"
-                                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
-                            >
-                                Live demo
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </main>
+        <div>
+            <Features />
+        </div>
+        <div>
+            <Display />
+        </div>
+        <div>
+            <Footer />
         </div>
     </div>
 </template>
 
 <script>
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
+import { MenuIcon, XIcon } from "@heroicons/vue/outline";
+import Features from "./Public/Features";
+import Display from "./Public/Display";
+import Footer from "./Public/Footer";
 import Nav from "./Public/Shared/Nav";
+
+const navigation = [
+    { name: "Product", href: "#" },
+    { name: "Price", href: "/price" },
+    { name: "Marketplace", href: "#" },
+    { name: "Company", href: "#" },
+];
 
 export default {
     components: {
+        Popover,
+        PopoverButton,
+        PopoverPanel,
+        MenuIcon,
+        XIcon,
+        Features,
+        Display,
+        Footer,
         Nav,
+    },
+    setup() {
+        return {
+            navigation,
+        };
     },
 };
 </script>
